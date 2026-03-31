1bf1232f1f090fbe0ac834ab1c1d696c312b610d
skpandya1212 <skpandya1212@gmail.com>
2026-03-31 13:57:50 +0530

    add option and localhost .

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,56 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth.routes.js";
-import productRoutes from "./routes/product.routes.js";
-import cartRoutes from "./routes/cart.routes.js";
-import orderRoutes from "./routes/order.routes.js";
-import adminRoutes from "./routes/admin.routes.js";
-import sellerRoutes from "./routes/seller.routes.js";
-import cors from 'cors';
+  import express from "express";
+  import dotenv from "dotenv";
+  import connectDB from "./config/db.js";
+  import authRoutes from "./routes/auth.routes.js";
+  import productRoutes from "./routes/product.routes.js";
+  import cartRoutes from "./routes/cart.routes.js";
+  import orderRoutes from "./routes/order.routes.js";
+  import adminRoutes from "./routes/admin.routes.js";
+  import sellerRoutes from "./routes/seller.routes.js";
+  import cors from 'cors';
 
-dotenv.config();
+  dotenv.config();
 
-// Connect Database
-connectDB();
+  // Connect Database
+  connectDB();
 
-const app = express();
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "https://cartify-nine-chi.vercel.app",
-  "https://cartify-gu4h.vercel.app"
-];
+  const app = express();
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://cartify-nine-chi.vercel.app",
+    "https://cartify-gu4h.vercel.app"
+  ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-app.use(express.json());
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
 
-app.get("/", (req, res) => {
-  res.json({ message: " E-Commerce API Running" });
-});
+  app.options("*", cors());
+  app.use(express.json());
 
-app.use("/uploads", express.static("uploads"));
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/seller", sellerRoutes);
+  app.get("/", (req, res) => {
+    res.json({ message: " E-Commerce API Running" });
+  });
 
-const PORT = process.env.PORT || 5000;
+  app.use("/uploads", express.static("uploads"));
+  app.use("/api/auth", authRoutes);
+  app.use("/api/products", productRoutes);
+  app.use("/api/cart", cartRoutes);
+  app.use("/api/orders", orderRoutes);
+  app.use("/api/admin", adminRoutes);
+  app.use("/api/seller", sellerRoutes);
 
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+  });
